bc19964325ab44d7636da7ab67527a66fc9ddc57
johnlindquist <johnlindquist@gmail.com>
2021-01-06 09:10:57 -0700

    testing script install

---
+++ public/scripts/johnlindquist/install-joke.js
@@ -0,0 +1,9 @@
+// Description: Logs out a Dad Joke from icanhazdadjoke.com
+
+let response = await get(`https://icanhazdadjoke.com/`, {
+  headers: {
+    Accept: 'text/plain',
+  },
+})
+
+console.log(response.data)
---
+++ src/pages/scripts/[user].tsx
@@ -0,0 +1,84 @@
+import {readdirSync, readFileSync} from 'fs'
+import path from 'path'
+import {useRouter} from 'next/router'
+
+type Script = {
+  file: string
+  command: string
+  content: string
+  url: string
+}
+
+export default function Script(props: any) {
+  const {scripts} = props
+
+  const router = useRouter()
+
+  return (
+    <div>
+      {scripts.map((script: Script) => {
+        return (
+          <div key={script.file}>
+            <h2>{script.file}</h2>
+            <div>{script.content}</div>
+            <a
+              href={
+                `simple://new ` +
+                script.command +
+                ' --url ' +
+                window.location.origin +
+                script.url
+              }
+            >
+              Install {script.command}
+            </a>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export async function getStaticProps(context: any) {
+  const {params} = context
+  const {user} = params
+  const scriptNames = readdirSync(
+    path.join(process.cwd(), '/public/scripts/', user),
+  )
+
+  const scripts = scriptNames.map((file) => {
+    const content = readFileSync(
+      path.join(process.cwd(), '/public/scripts/', user, file),
+      {encoding: 'utf8'},
+    )
+
+    const url = `/scripts/${user}/${file}`
+    return {
+      file,
+      command: file.replace('.js', ''),
+      content,
+      url,
+    }
+  })
+
+  return {
+    props: {scripts}, // will be passed to the page component as props
+  }
+}
+
+export async function getStaticPaths() {
+  const users = readdirSync(path.join(process.cwd(), '/public/scripts'))
+
+  const paths = users.map((user) => `/scripts/` + user)
+  return {
+    paths,
+    fallback: false,
+  }
+  //   const tags: any = await import('./tags.json').then((data) => data.default)
+  //   const tagSlugs: [string] = tags.map(({slug}: {slug: string}) => slug).sort()
+  //   const paths = tagSlugs.map((tag) => ({params: {tag}}))
+  //   return {
+  //     paths,
+  //     fallback: false,
+  //   }
+}
