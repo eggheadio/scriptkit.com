95dec9d7c17ac392c61c6772c240996a7bbf614b
johnlindquist <johnlindquist@gmail.com>
2021-02-02 10:00:06 -0700

    color-convert

---
+++ public/scripts/johnlindquist/color-convert.js
@@ -0,0 +1,49 @@
+// Menu: Convert Colors
+// Description: Converts colors between rgb, hex, etc
+// Author: John Lindquist
+// Twitter: @johnlindquist
+
+let {default: convert} = await npm('color-convert')
+
+let conversion = await arg('Enter color:', {
+  choices: (input) => {
+    if (input.startsWith('#')) {
+      return ['rgb', 'cmyk', 'hsl'].map((type) => {
+        let value = convert.hex[type](input).toString()
+
+        return {
+          name: type + ': ' + value,
+          value,
+          info: `<div style="background-color:${input}; width:100px; height:100px;">
+          ${input}
+          </div>`,
+        }
+      })
+    }
+
+    //two or more lowercase
+    if (input.match(/^[a-z]{2,}/)) {
+      return ['rgb', 'hex', 'cmyk', 'hsl']
+        .map((type) => {
+          try {
+            let value = convert.keyword[type](input).toString()
+
+            return {
+              name: type + ': ' + value,
+              value,
+              info: `<div style="background-color:${input}">
+              ${input}
+              </div>`,
+            }
+          } catch (error) {
+            return ''
+          }
+        })
+        .filter(Boolean)
+    }
+
+    return []
+  },
+})
+
+copy(conversion)
