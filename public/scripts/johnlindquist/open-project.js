58c7a7193a9a883a3b1ac6d6b18dbd6fdd4cf2a1
johnlindquist <johnlindquist@gmail.com>
2021-02-04 09:06:39 -0700

    open-project

---
+++ public/scripts/johnlindquist/open-project.js
@@ -0,0 +1,14 @@
+// Menu: Open Project
+// Description: Select from a list of projects to open
+// Author: John Lindquist
+// Twitter: @johnlindquist
+
+let project = await arg('Open project:', {
+  choices: [
+    '~/.simple',
+    '~/projects/simpleapp',
+    '~/projects/simplescripts.dev',
+  ],
+})
+
+edit(project)
