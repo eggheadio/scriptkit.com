13c408bfce71a1ddaee4118f0e82c9f36993fe54
johnlindquist <johnlindquist@gmail.com>
2021-03-05 14:24:52 -0700

    fixing chrome-tab

--- public/scripts/johnlindquist/chrome-tab.js
+++ public/scripts/johnlindquist/chrome-tab.js
@@ -2,23 +2,6 @@
 // Author: John Lindquist
 // Twitter: @johnlindquist
 
-applescript(`
-set address to "${await arg('Enter url:')}"
-
-tell application "Google Chrome"
-    activate
-        if not (exists window 1) then reopen
-        repeat with w in windows
-                set i to 1
-                repeat with t in tabs of w
-                if URL of t contains address then
-                        set active tab index of w to i
-                        set index of w to 1
-                        return
-                end if
-                set i to i + 1
-                end repeat
-        end repeat
-        open location \"http://\" & address
-end tell
-`)
+let {focusTab} = await kit('chrome')
+let url = await arg('Enter url:')
+focusTab(url)
