245a6ca65107c2536fcd196cb592537710f0d41f
johnlindquist <johnlindquist@gmail.com>
2021-03-15 17:23:44 -0600

    refactor speed-reader

--- public/scripts/johnlindquist/speed-reader.js
+++ public/scripts/johnlindquist/speed-reader.js
@@ -3,34 +3,18 @@
 // Author: John Lindquist
 // Twitter: @johnlindquist
 
-if (env.SIMPLE_CONTEXT === 'app') {
-  showMarkdown(`
-    Sorry, this script is only intended for the terminal.
+let wpm = 1000 * (60 / (await arg('Enter words per minute:')))
 
-    You can run it with:
+let text = await paste()
+text = text
+  .trim()
+  .split(' ')
+  .filter(Boolean)
+  .flatMap((sentence) => sentence.trim().split(' '))
 
-    \`~/.simple/bin/simple speed-reader\`
+let i = 0
 
-    Or add \`~/.simple/bin\` to your $PATH and run \`speed-reader\`
-    `)
-} else {
-  let {default: readline} = await npm('readline')
-
-  let wpm = 1000 * (60 / (await arg('Enter words per minute:')))
-
-  let text = await paste()
-  text = text
-    .trim()
-    .split(' ')
-    .filter(Boolean)
-    .flatMap((sentence) => sentence.trim().split(' '))
-
-  let i = 0
-
-  let id = setInterval(() => {
-    readline.clearLine(stdout, 0)
-    readline.cursorTo(stdout, 0)
-    stdout.write(text[i++])
-    if (i >= text.length) clearInterval(id)
-  }, wpm)
-}
+let id = setInterval(() => {
+  setPromptText(` ${text[i++]}`)
+  if (i >= text.length) clearInterval(id)
+}, wpm)
