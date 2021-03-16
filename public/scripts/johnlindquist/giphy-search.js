716ffb12411fb6d5755b217b2893e846830d41ec
johnlindquist <johnlindquist@gmail.com>
2021-03-16 09:13:24 -0600

    updating giphy-search

--- public/scripts/johnlindquist/giphy-search.js
+++ public/scripts/johnlindquist/giphy-search.js
@@ -1,26 +1,41 @@
-// Menu: Giphy
-// Description: Search giphy
+// Menu: Giphy Search
+// Description: Search giphy. Paste markdown link.
 // Author: John Lindquist
 // Twitter: @johnlindquist
+let download = await npm('image-downloader')
+let queryString = await npm('query-string')
+let {setSelectedText} = await kit('text')
 
-let GIPHY_API_KEY = await env('GIPHY_API_KEY', {
-  info: `<a href="https://developers.giphy.com/dashboard/">https://developers.giphy.com/dashboard/</a>`,
-})
+if (!env.GIPHY_API_KEY) {
+  show(
+    `<div class="p-4">
+      <div>
+        Grab an API Key from the Giphy dev dashboard:
+      </div>
+      <a href="https://developers.giphy.com/dashboard/">Here</a>
+    </div>`,
+  )
+}
+let GIPHY_API_KEY = await env('GIPHY_API_KEY')
 
 let search = (q) =>
   `https://api.giphy.com/v1/gifs/search?api_key=${GIPHY_API_KEY}&q=${q}&limit=10&offset=0&rating=g&lang=en`
 
-let gifUrl = await arg('Search giphy:', async (input) => {
+let {input, url} = await arg('Search giphy:', async (input) => {
   if (!input) return []
-  let {data} = await get(search(input))
+  let query = search(input)
+  let {data} = await get(query)
 
   return data.data.map((gif) => {
     return {
       name: gif.title.trim() || gif.slug,
-      value: gif.images.downsized_medium.url,
-      info: `<img src="${gif.images.downsized_medium.url}"/>`,
+      value: {
+        input,
+        url: gif.images.downsized_medium.url,
+      },
+      img: gif.images.downsized_medium.url,
     }
   })
 })
 
-setSelectedText(gifUrl)
+setSelectedText(`![${input}](${url})`)
