db2f8b344ff7d30b5857cdd3a210bb436ea5544f
johnlindquist <johnlindquist@gmail.com>
2021-04-09 17:00:40 -0600

    adding scripts api and util

---
+++ src/pages/api/scripts/[user].ts
@@ -0,0 +1,13 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import {NextApiRequest, NextApiResponse} from 'next'
+import getUserScripts from 'utils/get-user-scripts'
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  res.statusCode = 200
+  const {user} = req.query
+
+  const scripts = getUserScripts(user as string)
+
+  res.send(scripts)
+}
