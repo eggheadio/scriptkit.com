a3805d095c5063e7fdb37b1f04a17870c179ca31
johnlindquist <johnlindquist@gmail.com>
2021-04-20 16:37:53 -0600

    adding confirm page

---
+++ src/pages/confirm.tsx
@@ -0,0 +1,13 @@
+export default function Confirm() {
+  return (
+    <div className="flex flex-col justify-center items-center p-32">
+      <h2 className="text-xl pb-2">
+        Script Kit Newletter Subscription Confirmed!
+      </h2>
+      <div className="flex flex-col items-center">
+        <p>Thanks for signing up and confirming.</p>{' '}
+        <p>Expect some newsletters in your inbox soon! 👀</p>
+      </div>
+    </div>
+  )
+}
