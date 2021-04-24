4e17e626271a28b3da00048e786eb0c71709850e
johnlindquist <johnlindquist@gmail.com>
2021-04-24 09:57:34 -0600

    fixing shortcuts

--- public/scripts/johnlindquist/focus-twitter.js
+++ public/scripts/johnlindquist/focus-twitter.js
@@ -1,6 +1,6 @@
 // Description: Launch Twitter in Chrome. If Twitter is already open, switch to that tab.
 // Author: John Lindquist
 // Twitter: @johnlindquist
-// Shortcut: Alt+T
+// Shortcut: opt t
 //requires the "chrome-tab" script
 await run('chrome-tab', 'twitter.com')
