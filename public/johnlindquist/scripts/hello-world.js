2c9cbd58b6a2d372d1eb21841bc7cb3f8bcac30d
johnlindquist <johnlindquist@gmail.com>
2021-05-26 13:47:55 -0600

    rming johnlindquist dir

--- public/johnlindquist/scripts/hello-world.js
+++
@@ -1,8 +0,0 @@
-// Menu: Hello World
-// Description: Enter an name, speak it back
-// Author: John Lindquist
-// Twitter: @johnlindquist
-
-let {say} = await kit('speech')
-let name = await arg(`What's your name?`)
-say(`Hello, ${name}!`)
--- public/johnlindquist/scripts/sound-settings.js
+++
@@ -1,6 +0,0 @@
-// Menu: Open Sound Prefs
-// Description: Open the Sound prefs panel
-// Author: John Lindquist
-// Twitter: @johnlindquist
-
-exec(`open /System/Library/PreferencePanes/Sound.prefPane`)
