9c18ecad63369101729a76bfa15c848e0aa9234f
Vojta Holik <vojta@egghead.io>
2021-06-02 15:19:08 +0200

    tell them

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -4,20 +4,21 @@ import Logo from 'images/logo.svg'
 
 const Navigation = () => {
   return (
-    <nav className="w-full flex items-center justify-between py-5 lg:px-0 px-5 max-w-screen-lg mx-auto font-medium">
+    <nav className="flex items-center justify-between w-full max-w-screen-lg px-5 py-5 mx-auto font-medium lg:px-0">
       <Link href="/">
         <a className="flex items-center">
-          <div className="w-10 h-10 rounded-lg bg-gradient-to-t from-amber-400 to-yellow-300 text-black font-bold flex items-center justify-center tracking-tighter">
+          <div className="flex items-center justify-center w-10 h-10 font-bold tracking-tighter text-black rounded-lg bg-gradient-to-t from-amber-400 to-yellow-300">
             <Logo />
           </div>
-          <div className="pl-2 font-bold text-lg leading-tighter">
-            Script Kit
+          <div className="pl-2">
+            <div className="text-lg font-bold leading-tighter">Script Kit</div>
+            <div className="text-sm opacity-80">by John Lindquist</div>
           </div>
         </a>
       </Link>
       <div className="flex items-center space-x-5">
         <Link href="/scripts" activeClassName="cursor-default bg-gray-900">
-          <a className="sm:text-base text-sm flex items-center space-x-1 sm:px-3 px-2 sm:py-2 py-1 rounded-lg hover:bg-gray-900 transition-all duration-100 ease-in-out opacity-90 hover:opacity-100">
+          <a className="flex items-center px-2 py-1 space-x-1 text-sm transition-all duration-100 ease-in-out rounded-lg sm:text-base sm:px-3 sm:py-2 hover:bg-gray-900 opacity-90 hover:opacity-100">
             Browse Scripts
           </a>
         </Link>
