624c8129dd765f13b489e9c4f5194f8100513e01
John Lindquist <johnlindquist@gmail.com>
2021-08-24 13:05:12 -0600

    fixing img and videos

--- src/components/discussion-post.tsx
+++ src/components/discussion-post.tsx
@@ -6,6 +6,7 @@ import theme from 'prism-react-renderer/themes/nightOwl'
 
 import {DiscussionProps} from 'utils/get-discussions'
 import CodeBlock from 'components/code-block'
+import rehypeRaw from 'rehype-raw'
 
 const DiscussionPost = ({
   discussion: {url, slug, title, body},
@@ -24,7 +25,21 @@ const DiscussionPost = ({
 
     <ReactMarkdown
       children={body}
+      rehypePlugins={[rehypeRaw]}
       components={{
+        p({children}) {
+          const c = String(children)
+
+          if (c.startsWith('http') && c.endsWith('.mp4')) {
+            return (
+              <video controls className="w-full">
+                <source src={c} type="video/mp4" />
+              </video>
+            )
+          }
+
+          return <p>{children}</p>
+        },
         code({node, inline, className, children, ...props}: any) {
           const match = /language-(\w+)/.exec(className || '')
           return !inline && match ? (
