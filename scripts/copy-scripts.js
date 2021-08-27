6f36d5b0958f4884e6b998b6be2e21743da57f8c
John Lindquist <johnlindquist@gmail.com>
2021-08-27 13:02:06 -0600

    working locally ...

--- scripts/copy-scripts.js
+++ scripts/copy-scripts.js
@@ -1,15 +1,6 @@
 let tree = await npm('tree-cli')
 
-console.log(`👮‍♀️ copy scripts`)
-let tmpScriptsPath = kitPath('tmp', 'scripts')
-
-console.log(`👀 tmpScriptsPath: ${tmpScriptsPath}`)
-let tmpTree = await tree({base: kitPath('tmp'), l: 2})
-console.log(`tmpTree report`)
-console.log(tmpTree.report)
-
-console.log(`kenvPath scripts:`)
-cp('-R', kenvPath('./scripts/*'), kitPath('tmp', 'scripts'))
+cp(kenvPath('./scripts/*'), kitPath('tmp', 'scripts'))
 
 console.log(`tmpTree report 2`)
 let res = await tree({base: kitPath('tmp'), l: 3})
