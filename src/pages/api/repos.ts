f9be7100effa84860c62d69df8b3540124bbfe42
John Lindquist <johnlindquist@gmail.com>
2021-08-27 12:11:16 -0600

    loggings lots of trees

--- src/pages/api/repos.ts
+++ src/pages/api/repos.ts
@@ -3,6 +3,11 @@
 import {NextApiRequest, NextApiResponse} from 'next'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  let {default: tree} = await import('tree-cli')
+  let tmpTree = await tree({l: 4})
+  console.log(`tmpTree report`)
+  console.log(tmpTree.report)
+
   await import('@johnlindquist/kit')
   res.json(await kit('repos'))
 }
