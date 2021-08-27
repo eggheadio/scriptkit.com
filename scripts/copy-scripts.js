30abc6649759f0c3cc9362579d1720b20918b6d1
John Lindquist <johnlindquist@gmail.com>
2021-08-26 22:28:42 -0600

    fixing copy-scripts

--- scripts/copy-scripts.js
+++ scripts/copy-scripts.js
@@ -4,23 +4,15 @@ console.log(`👮‍♀️ copy scripts`)
 let tmpScriptsPath = kitPath('tmp', 'scripts')
 
 console.log(`👀 tmpScriptsPath: ${tmpScriptsPath}`)
-let tmpTree = await tree(kitPath(), {l: 2})
+let tmpTree = await tree({base: kitPath('tmp'), l: 2})
+console.log(`tmpTree report`)
 console.log(tmpTree.report)
 
 console.log(`kenvPath scripts:`)
-cp('-r', kenvPath('./scripts/*'), kitPath('tmp', 'scripts', '*'))
-console.log(await readdir(kenvPath(`scripts`)))
+cp('-R', kenvPath('./scripts/*'), kitPath('tmp', 'scripts'))
 
-console.log(`👀 tmpScriptsPath:`)
-console.log(await readdir(tmpScriptsPath))
-console.log(`\n\n\n`)
-
-console.log(`./scripts:`)
-console.log(await readdir(`./scripts`))
-cp('-r', './scripts/*', kitPath('tmp', 'scripts', '*'))
-
-console.log(`KIT PATH`)
-let res = await tree({base: kitPath(), l: 3})
+console.log(`tmpTree report 2`)
+let res = await tree({base: kitPath('tmp'), l: 3})
 console.log(res?.report)
 
 export {}
