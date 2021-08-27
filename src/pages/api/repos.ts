4a539d3c307fb5026a947abe8870325d1de1be80
John Lindquist <johnlindquist@gmail.com>
2021-08-27 14:33:05 -0600

    import

--- src/pages/api/repos.ts
+++ src/pages/api/repos.ts
@@ -1,8 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 import {NextApiRequest, NextApiResponse} from 'next'
+import '@johnlindquist/kit'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  await import('@johnlindquist/kit')
   res.json(await kit('repos'))
 }
