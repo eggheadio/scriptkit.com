db685f54c8b95ab75e9c983fa50ad0931fb62ab1
John Lindquist <johnlindquist@gmail.com>
2021-08-26 20:49:17 -0600

    attempting tree

--- scripts/copy-scripts.js
+++ scripts/copy-scripts.js
@@ -2,19 +2,30 @@ console.log(`👮‍♀️ copy scripts`)
 console.log(kitPath('src'))
 let tmpScriptsPath = kitPath('tmp', 'scripts')
 
+console.log(`👀 tmpScriptsPath:`)
+console.log(await readdir(tmpScriptsPath))
+
 console.log(`kenvPath scripts:`)
 cp('-r', kenvPath('./scripts'), tmpScriptsPath)
 console.log(await readdir(kenvPath(`scripts`)))
 
+console.log(`👀 tmpScriptsPath:`)
+console.log(await readdir(tmpScriptsPath))
 console.log(`\n\n\n`)
 
-console.log(`.scripts:`)
-console.log(await readdir(`.scripts`))
+console.log(`./scripts:`)
+console.log(await readdir(`./scripts`))
 cp('-r', './scripts', tmpScriptsPath)
 
+console.log(`👀 tmpScriptsPath:`)
+console.log(await readdir(tmpScriptsPath))
 console.log(`\n\n\n`)
 
-console.log(`👀 tmpScriptsPath:`)
-console.log(tmpScriptsPath)
+let tree = await npm('tree-cli')
+
+console.log(`KIT PATH`)
+let res = await tree({base: kitPath()})
+console.log(res?.data)
+console.log(res?.report)
 
 export {}
