fd46af8ca4e0b040cdf0974a5e2cff811322b66d
John Lindquist <johnlindquist@gmail.com>
2021-08-27 17:26:33 -0600

    kit

--- src/pages/api/repos.ts
+++ src/pages/api/repos.ts
@@ -2,7 +2,6 @@
 
 import {NextApiRequest, NextApiResponse} from 'next'
 import '@johnlindquist/kit'
-import '@johnlindquist/kit/tmp/scripts/repos.js'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   res.json(await kit('repos'))
