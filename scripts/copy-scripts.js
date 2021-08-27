5ebce087aa931dd5dfa57d5249b6e0fce67434a1
John Lindquist <johnlindquist@gmail.com>
2021-08-26 20:39:36 -0600

    update copy-scripts

--- scripts/copy-scripts.js
+++ scripts/copy-scripts.js
@@ -2,7 +2,19 @@ console.log(`👮‍♀️ copy scripts`)
 console.log(kitPath('src'))
 let tmpScriptsPath = kitPath('tmp', 'scripts')
 
+console.log(`kenvPath scripts:`)
 cp('-r', kenvPath('./scripts'), tmpScriptsPath)
-console.log(kenvPath('scripts'))
+console.log(await readdir(kenvPath(`scripts`)))
+
+console.log(`\n\n\n`)
+
+console.log(`.scripts:`)
+console.log(await readdir(`.scripts`))
+cp('-r', './scripts', tmpScriptsPath)
+
+console.log(`\n\n\n`)
+
+console.log(`👀 tmpScriptsPath:`)
+console.log(tmpScriptsPath)
 
 export {}
