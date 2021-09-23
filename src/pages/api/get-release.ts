8dbdb714428385ae15f10e2efcab22a260a31e09
John Lindquist <johnlindquist@gmail.com>
2021-09-23 08:30:44 -0600

    testing opengraph image generator

--- src/pages/api/get-release.ts
+++ src/pages/api/get-release.ts
@@ -3,6 +3,7 @@ import '@johnlindquist/kit'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const {channel = 'main'} = req.query
+
   let url = await kit(`get-kit-release ${channel}`)
 
   res.redirect(url)
