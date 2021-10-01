ba8bca3556d272a09a8441fe5ccf984cc361ddaa
John Lindquist <johnlindquist@gmail.com>
2021-10-01 13:34:49 -0600

    fix(opengraph): testing blog og image

--- src/pages/api/cloudinary-cache.ts
+++ src/pages/api/cloudinary-cache.ts
@@ -1,14 +1,21 @@
 import {NextApiRequest, NextApiResponse} from 'next'
 import {v2 as cloudinary} from 'cloudinary'
+import qs from 'query-string'
+import slugify from 'slugify'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const {user, title} = req.query
   cloudinary.config({
     cloud_name: 'johnlindquist',
     api_key: process.env.CLOUDINARY_KEY,
     api_secret: process.env.CLOUDINARY_SECRET,
   })
 
-  const source = `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/opengraph?${req.query}`
+  const source = `https://${
+    process.env.NEXT_PUBLIC_VERCEL_URL
+  }/api/opengraph/kit/${user}/${slugify(title as string, {
+    lower: true,
+  })}.png?${qs.stringify(req.query)}`
 
   const imageUrl = cloudinary.url(source, {
     type: 'fetch',
