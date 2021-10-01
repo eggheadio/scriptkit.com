6142f28ff304be4f846bd6d9a4d515189a419a4c
John Lindquist <johnlindquist@gmail.com>
2021-10-01 17:09:01 -0600

    fix(opengraph): testing blog og image

--- src/pages/[user]/scripts/[script].tsx
+++ src/pages/[user]/scripts/[script].tsx
@@ -4,34 +4,52 @@ import Layout from 'layouts'
 import ScriptDetail from 'components/pages/scripts/detail'
 import {getScript, getScriptPaths, Script} from 'utils/get-user-scripts'
 import Meta from 'components/meta'
+import {Discussion, getUserShares} from 'utils/get-discussions'
+import DiscussionPost from 'components/discussion-post'
 
-const ScriptComponent: FunctionComponent<{script: Script}> = ({script}) => {
-  return (
-    <Layout
-      meta={{
-        title: `${script.command} script by ${script.user}`,
-        description: script.description || undefined,
-      }}
-    >
-      <Meta
-        user={script.user}
-        title={script.description || script.command}
-        twitter={script.twitter}
-      />
-      <div className="max-w-screen-lg w-full mx-auto">
-        <ScriptDetail {...script} />
-      </div>
-    </Layout>
-  )
-}
+const ScriptComponent: FunctionComponent<{script: Script; share: Discussion}> =
+  ({script, share}) => {
+    return (
+      <Layout
+        meta={{
+          title: `${script.command} script by ${script.user}`,
+          description: script.description || undefined,
+        }}
+      >
+        <Meta
+          user={script.user}
+          title={script.description || script.command}
+          twitter={script.twitter}
+        />
+        <div className="max-w-screen-lg w-full mx-auto">
+          {share ? (
+            <DiscussionPost discussion={share} />
+          ) : (
+            <ScriptDetail {...script} />
+          )}
+        </div>
+      </Layout>
+    )
+  }
 
 export async function getStaticProps(context: any) {
   const {params} = context
   const {script, user} = params
 
+  const shares = await getUserShares(user)
+  const userShare = shares.find((s) => s.slug === script)
+
+  const userScript = await getScript(user, script)
+
+  console.log({
+    userScript,
+    userShare,
+  })
+
   return {
     props: {
-      script: await getScript(user, script),
+      script: userScript,
+      share: userShare,
     },
   }
 }
