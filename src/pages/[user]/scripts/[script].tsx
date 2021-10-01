15c0c13c0180893924f74a6bc80b165291062f6b
John Lindquist <johnlindquist@gmail.com>
2021-09-30 18:14:07 -0600

    fix(opengraph): testing opengraph image on blog

--- src/pages/[user]/scripts/[script].tsx
+++ src/pages/[user]/scripts/[script].tsx
@@ -3,6 +3,7 @@ import {FunctionComponent} from 'react'
 import Layout from 'layouts'
 import ScriptDetail from 'components/pages/scripts/detail'
 import {getScript, getScriptPaths, Script} from 'utils/get-user-scripts'
+import Meta from 'components/meta'
 
 const ScriptComponent: FunctionComponent<{script: Script}> = ({script}) => {
   return (
@@ -12,6 +13,7 @@ const ScriptComponent: FunctionComponent<{script: Script}> = ({script}) => {
         description: script.description || undefined,
       }}
     >
+      <Meta user={script.user} title={script.description} />
       <div className="max-w-screen-lg w-full mx-auto">
         <ScriptDetail {...script} />
       </div>
