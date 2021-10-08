d49c2efb7ba6bc0b33ef99a0a869450fe1415cc2
John Lindquist <johnlindquist@gmail.com>
2021-10-08 12:16:33 -0600

    fix(community): adding community page

--- scripts/write-discussion-data.ts
+++ scripts/write-discussion-data.ts
@@ -17,7 +17,7 @@ let endpoint = 'https://api.github.com/graphql'
 let categoryKey = await arg('Category', Object.keys(Category))
 let category = {
   name: categoryKey,
-  value: Category[categoryKey as Category],
+  value: Category[categoryKey as any],
 }
 
 let client = new GraphQLClient(endpoint, {
