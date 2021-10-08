6e61ab3e3f27e9f56e7eda15a49a286335a25544
John Lindquist <johnlindquist@gmail.com>
2021-10-08 16:27:14 -0600

    fix(community): adding community page

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -24,7 +24,7 @@ function Navigation() {
     <Disclosure as="nav">
       {({open}) => (
         <>
-          <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+          <div className="w-full max-w-screen-lg px-5 py-2 mx-auto font-medium lg:px-0">
             <div className="relative flex items-center justify-between h-16">
               <Link href="/">
                 <a className="flex items-center">
@@ -52,7 +52,7 @@ function Navigation() {
               </div>
               <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-end">
                 <div className="hidden sm:block sm:ml-6">
-                  <div className="flex space-x-4">
+                  <div className="flex space-x-2">
                     {navigation.map((item) => (
                       <a
                         key={item.name}
@@ -61,7 +61,7 @@ function Navigation() {
                           item.current
                             ? 'bg-gray-900 text-white'
                             : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                          'px-3 py-2 rounded-md text-sm font-medium',
+                          'px-3 py-2 rounded-md text-md font-medium leading-tighter tracking-tight',
                         )}
                         aria-current={item.current ? 'page' : undefined}
                       >
