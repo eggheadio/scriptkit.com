9cddb2a0a037937263a82597d6bc134cc3482559
John Lindquist <johnlindquist@gmail.com>
2021-10-08 23:00:48 -0600

    fix(discussion): build on discussion

---
+++ src/pages/api/discussion-updated.ts
@@ -0,0 +1,18 @@
+import {NextApiRequest, NextApiResponse} from 'next'
+
+import axios from 'axios'
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  console.log(req.body)
+
+  const {action} = req.body
+
+  if (['edited', 'created', 'deleted'].includes(action)) {
+    const response = await axios.post(
+      `https://api.vercel.com/v1/integrations/deploy/prj_zRKFY6s5AgVZAn563mHGiMul3IAi/fcS0xcULP0`,
+    )
+
+    res.send(response.data)
+  }
+
+  res.send(req.body)
+}
