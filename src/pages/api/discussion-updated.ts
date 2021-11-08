ce41201570b46cd191bf1a9c33dcc56d580adc24
John Lindquist <johnlindquist@gmail.com>
2021-11-08 06:33:46 -0700

    fix(webhook): ignore comments

--- src/pages/api/discussion-updated.ts
+++ src/pages/api/discussion-updated.ts
@@ -4,9 +4,9 @@ import axios from 'axios'
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   console.log(req.body)
 
-  const {action} = req.body
+  const {action, comment} = req.body
 
-  if (['edited', 'created', 'deleted'].includes(action)) {
+  if (['edited', 'created', 'deleted'].includes(action) && !comment) {
     const response = await axios.post(
       `https://api.vercel.com/v1/integrations/deploy/prj_zRKFY6s5AgVZAn563mHGiMul3IAi/fcS0xcULP0`,
       {},
