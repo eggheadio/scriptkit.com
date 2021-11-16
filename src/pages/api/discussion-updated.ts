9736fcb2b5a8d0e1683e7e91cf107cb58ffa74d6
John Lindquist <johnlindquist@gmail.com>
2021-11-15 17:18:15 -0700

    feat(docs): use workflow for build

--- src/pages/api/discussion-updated.ts
+++ src/pages/api/discussion-updated.ts
@@ -4,15 +4,35 @@ import axios from 'axios'
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   console.log(req.body)
 
-  const {action, comment} = req.body
+  const {action, comment, discussion} = req.body
 
   if (['edited', 'created', 'deleted'].includes(action) && !comment) {
-    const response = await axios.post(
-      `https://api.vercel.com/v1/integrations/deploy/prj_zRKFY6s5AgVZAn563mHGiMul3IAi/fcS0xcULP0`,
-      {},
-    )
+    console.log({
+      action,
+      discussion,
+    })
+    if (discussion?.category?.name === 'Docs') {
+      let requestOptions = {
+        headers: {
+          Authorization: `token ${process.env.WORKFLOW_TOKEN}`,
+          Accept: 'application/vnd.github.v3+json',
+        },
+      }
+      const response = await axios.post(
+        `https://api.github.com/repos/johnlindquist/kit-docs/actions/workflows/15293663`,
+        {ref: 'main'},
+        requestOptions,
+      )
 
-    res.send(response.data)
+      res.send(response.data)
+    } else {
+      const response = await axios.post(
+        `https://api.vercel.com/v1/integrations/deploy/prj_zRKFY6s5AgVZAn563mHGiMul3IAi/fcS0xcULP0`,
+        {},
+      )
+
+      res.send(response.data)
+    }
   } else {
     res.send(req.body)
   }
