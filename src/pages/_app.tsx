451e9b389bfb88980060172499dc8061d212c1b6
Vojta Holik <vojta@egghead.io>
2022-01-03 16:14:40 +0100

    feature kit showcase course on homepage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,18 +4,12 @@ import '../styles/globals.css'
 import 'focus-visible'
 import {DefaultSeo} from 'next-seo'
 import SEO from '../../next-seo.json'
-import {ViewerProvider} from 'context/viewer-context'
-import {ConvertkitProvider} from 'hooks/use-convertkit'
 
 function MyApp({Component, pageProps}: AppProps) {
   return (
     <>
-      <ViewerProvider>
-        <ConvertkitProvider>
-          <DefaultSeo {...SEO} />
-          <Component {...pageProps} />
-        </ConvertkitProvider>
-      </ViewerProvider>
+      <DefaultSeo {...SEO} />
+      <Component {...pageProps} />
     </>
   )
 }
