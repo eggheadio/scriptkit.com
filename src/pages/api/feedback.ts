afbe8783faf48b4da8ce1e1bcf8ae290a4963e43
John Lindquist <johnlindquist@gmail.com>
2022-01-24 21:45:03 -0700

    fix(fns): remove zapier

--- src/pages/api/feedback.ts
+++ src/pages/api/feedback.ts
@@ -2,7 +2,7 @@ import {NextApiRequest, NextApiResponse} from 'next'
 import '@johnlindquist/globals'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  await post(`https://hooks.zapier.com/hooks/catch/3863955/b9x0a39`, req.body)
+  // await post(`https://hooks.zapier.com/hooks/catch/3863955/b9x0a39`, req.body)
 
   res.send(req.body)
 }
