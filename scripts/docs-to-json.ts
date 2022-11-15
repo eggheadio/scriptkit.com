48d368d0cec7527ad7e72ed19b8c7c10267d7080
John Lindquist <johnlindquist@gmail.com>
2022-11-15 13:22:31 -0700

    fix(clean): clean stray files

--- scripts/docs-to-json.ts
+++ scripts/docs-to-json.ts
@@ -4,8 +4,6 @@ import {getDiscussions, Category} from '../src/lib/get-discussions.js'
 let run = async () => {
   console.log(`Starting discussion json generation:`)
 
-  console.log({getDiscussions})
-
   let jsonfile = await npm('jsonfile')
   let docs = await getDiscussions(Category.Guide)
   let outfile = path.resolve(`./public/data/docs.json`)
