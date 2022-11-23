8f2f9b0461de8e236a9fa14ff843c4a32d3224df
John Lindquist <johnlindquist@gmail.com>
2022-11-23 09:27:50 -0700

    feat(add): get-install-count

---
+++ src/pages/api/get-install-count.ts
@@ -0,0 +1,38 @@
+import {Octokit} from '@octokit/rest'
+import {NextApiRequest, NextApiResponse} from 'next'
+
+let octokit = new Octokit({
+  auth: process.env.GITHUB_SCRIPTKITCOM_TOKEN,
+})
+
+type Asset = {download_count: number}
+
+let getReleaseCount = async () => {
+  let releases = await octokit.paginate('GET /repos/{owner}/{repo}/releases', {
+    owner: 'johnlindquist',
+    repo: 'kitapp',
+  })
+
+  let dmgs = releases.flatMap(
+    (release) =>
+      release.assets.filter(
+        (asset) =>
+          asset.name.endsWith('.dmg') ||
+          asset.name.endsWith('.exe') ||
+          asset.name.endsWith('.AppImage'),
+      ) as Asset[],
+  )
+
+  let count = dmgs
+    .filter(Boolean)
+    .reduce((acc, asset) => (acc += asset.download_count), 0)
+
+  return count
+}
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  let count = await getReleaseCount()
+
+  res.statusCode = 200
+  res.json(count)
+}
