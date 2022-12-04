5bf2cc0b90ec6fa6f71cbd991e671a323f8d7f23
John Lindquist <johnlindquist@gmail.com>
2022-12-04 11:33:06 -0700

    fix(twitter): disable twitter count

--- src/pages/api/update-twitter-count.ts
+++ src/pages/api/update-twitter-count.ts
@@ -5,14 +5,14 @@ let octokit = new Octokit({
   auth: process.env.GITHUB_SCRIPTKITCOM_TOKEN,
 })
 
-import Twitter from 'twitter-lite'
+// import Twitter from 'twitter-lite'
 
-let client = new Twitter({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY as string,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET as string,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-})
+// let client = new Twitter({
+//   consumer_key: process.env.TWITTER_CONSUMER_KEY as string,
+//   consumer_secret: process.env.TWITTER_CONSUMER_SECRET as string,
+//   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+//   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+// })
 
 type Asset = {download_count: number}
 
@@ -45,25 +45,13 @@ import {NextApiRequest, NextApiResponse} from 'next'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   console.log(`🆙date twitter count`)
-  let count = await getReleaseCount()
-  let response = await client.post('account/update_profile', {
-    name: `John (${count}) Lindquist`,
-    //     description: `
-    // The number in my name updates when someone downloads Script Kit 🤓
-
-    // · scriptkit.com Creator
-    // · egghead.io founder
-    // · nap enthusiast
-    //   `,
-  })
-
-  console.log({count})
-
-  // await axios.post(`https://hooks.zapier.com/hooks/catch/3863955/b98o9wx/`, {
-  //   count,
-  //   timestamp: new Date().toUTCString(),
+  // let count = await getReleaseCount()
+  // let response = await client.post('account/update_profile', {
+  //   name: `John (${count}) Lindquist`,
   // })
 
+  // console.log({count})
+
   res.statusCode = 200
-  res.json({name: 'John Doe'})
+  res.json({name: 'disabled until twitter api is fixed'})
 }
