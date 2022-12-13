89b9a0483a7cbe314fce4a6d533d3143190e6861
John Lindquist <johnlindquist@gmail.com>
2022-12-13 08:41:04 -0700

    fix(twitter): update twitter count

--- src/pages/api/update-twitter-count.ts
+++ src/pages/api/update-twitter-count.ts
@@ -5,14 +5,14 @@ let octokit = new Octokit({
   auth: process.env.GITHUB_SCRIPTKITCOM_TOKEN,
 })
 
-// import Twitter from 'twitter-lite'
+import Twitter from 'twitter-lite'
 
-// let client = new Twitter({
-//   consumer_key: process.env.TWITTER_CONSUMER_KEY as string,
-//   consumer_secret: process.env.TWITTER_CONSUMER_SECRET as string,
-//   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-//   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-// })
+let client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY as string,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET as string,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+})
 
 type Asset = {download_count: number}
 
@@ -41,14 +41,19 @@ let getReleaseCount = async () => {
 
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+const getCachedReleaseCount = async () => {
+  return await axios.get('https://www.scriptkit.com/api/get-install-count')
+}
+
 import {NextApiRequest, NextApiResponse} from 'next'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   console.log(`🆙date twitter count`)
   // let count = await getReleaseCount()
-  // let response = await client.post('account/update_profile', {
-  //   name: `John (${count}) Lindquist`,
-  // })
+  let count = await getCachedReleaseCount()
+  let response = await client.post('account/update_profile', {
+    name: `John (${count}) Lindquist`,
+  })
 
   // console.log({count})
 
